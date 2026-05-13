ef3401751da2386599d55e767b7f2152595dd7f8
o.qosimov <oqdevpy@gmail.com>
2026-05-13 14:40:18 +0500

    Update Docker configuration and settings for improved database environment variables

--- config/settings.py
+++ config/settings.py
@@ -62,11 +62,11 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB'),
-        'USER': os.getenv('POSTGRES_USER'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-        'HOST': 'db',  # Docker Compose service name
-        'PORT': 5432,
+        'NAME': os.getenv('DB_NAME', os.getenv('POSTGRES_DB')),
+        'USER': os.getenv('DB_USER', os.getenv('POSTGRES_USER')),
+        'PASSWORD': os.getenv('DB_PASSWORD', os.getenv('POSTGRES_PASSWORD')),
+        'HOST': os.getenv('DB_HOST', 'host.docker.internal'),  # Docker Compose env yoki fallback
+        'PORT': os.getenv('DB_PORT', 5432),
     }
 }
 
